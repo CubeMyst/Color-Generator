f13d841d380696a1e74e1f330297fb4c68deda0a
YeiserDeveloper <yeiser192@gmail.com>
2023-04-30 03:03:29 -0400

    Feature Implementation

---
+++ README.md
@@ -0,0 +1,11 @@
+# ¡Bienvenido a mi generador de color aleatorio con React!
+
+!["Screenshot"][def]
+
+Este proyecto es un generador de colores aleatorios desarrollado en React. Utiliza la librería "**randomcolor**" para generar nuevos colores aleatorios cada vez que se presiona el botón "**Generar color**". Además, utiliza funciones de JavaScript para convertir el color en formato hexadecimal a su contraparte de contraste (blanco o negro), lo que permite mostrar un valor de texto legible en la pantalla. 
+
+El usuario puede copiar el valor del color generado en formato hexadecimal haciendo clic en el campo de entrada de texto correspondiente. El proyecto también incluye algunas características de estilo para el botón y la caja de color generada.
+
+La aplicación es fácil de usar: simplemente haz clic en el botón "**Generar color**" para obtener un nuevo color aleatorio y su valor hexadecimal correspondiente. 
+
+[def]: ./screenshot.png
--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,139 @@
-import { useState, useEffect, useRef } from 'react';
-import randomcolor from 'randomcolor';
-import './App.css';
+import { useState, useEffect, useRef, useCallback } from "react";
+import randomcolor from "randomcolor";
+import "./App.css";
 
-function App() {
-  const [currentColor, setCurrentColor] = useState('#000000');
-  const [contrastColor, setContrastColor] = useState('#FFFFFF');
-  const [hoverColor, setHoverColor] = useState<string | null>(null);
-  const hexRef = useRef(null);
+function App(): JSX.Element {
+  const [currentColor, setCurrentColor] = useState<string>("#000000");
+  const [contrastColor, setContrastColor] = useState<string>("#FFFFFF");
+  const hexRef = useRef<HTMLInputElement>(null);
 
-  useEffect(() => {
-    function getContrastColor(hexColor: String) {
-      const r = parseInt(hexColor.slice(1, 2), 16);
-      const g = parseInt(hexColor.slice(2, 4), 16);
-      const b = parseInt(hexColor.slice(4, 6), 16);
-
-      const luminance = (0.2126 * r + 0.7152 * g + 0.0722 * b) / 255;
-      return luminance > 0.5 ? '#000000' : '#FFFFFF';
-    }
-
-    const contrast= getContrastColor(currentColor);
-    setContrastColor(contrast);
-  }, [currentColor]);
-
-  function handleGenerateColor() {
-    const color = randomcolor();
-    setCurrentColor(color);
+  interface HSL {
+    hue: number;
+    saturation: number;
+    lightness: number;
   }
 
-  function handleCopyHex() {
+  const getContrastColor = useCallback((hexColor: string): '#FFFFFF' | '#000000' => {
+    if (!/^#[0-9A-Fa-f]{6}$/.test(hexColor)) {
+      throw new Error(`Invalid hex color value: ${hexColor}`);
+    }
+
+    const r = parseInt(hexColor.slice(1, 3), 16);
+    const g = parseInt(hexColor.slice(3, 5), 16);
+    const b = parseInt(hexColor.slice(5, 7), 16);
+
+    const hsl = rgbToHsl(r, g, b);
+    const luminance: number = hsl.lightness || 0;
+
+    return luminance < 0.5 ? "#FFFFFF" : "#000000";
+  }, []);
+
+  const rgbToHsl = useCallback((r: number, g: number, b: number): HSL => {
+    const red = r / 255;
+    const green = g / 255;
+    const blue = b / 255;
+
+    const maxValue = Math.max(red, green, blue);
+    const minValue = Math.min(red, green, blue);
+
+    let hue = 0;
+    let saturation = 0;
+    const lightness = (maxValue + minValue) / 2;
+
+    if (maxValue !== minValue) {
+      const delta = maxValue - minValue;
+      saturation = lightness > 0.5 ? delta / (2 - maxValue - minValue) : delta / (maxValue + minValue);
+
+      switch (maxValue) {
+        case red:
+          hue = (green - blue) / delta + (green < blue ? 6 : 0);
+          break;
+        case green:
+          hue = (blue - red) / delta + 2;
+          break;
+        case blue:
+          hue = (red - green) / delta + 4;
+          break;
+      }
+      hue /= 6;
+    }
+
+    return { hue, saturation, lightness };
+  }, []);
+
+  useEffect(() => {
+    const contrast = getContrastColor(currentColor);
+    setContrastColor(contrast);
+  }, [currentColor, getContrastColor]);
+
+  // Genera un nuevo color aleatorio y lo establece como el color actual
+  const handleGenerateColor = useCallback((): void => {
+    const randomColor: string = randomcolor();
+    setCurrentColor(randomColor);
+  }, [])
+
+  const handleCopyHex = useCallback((): void => {
     navigator.clipboard.writeText(currentColor);
-    // @ts-ignore
-    hexRef.current.focus();
-    // @ts-ignore
+    hexRef.current?.focus();
+    hexRef.current?.select();
+
+    if (hexRef.current) {
     hexRef.current.select();
+
+    if (Notification && (Notification.permission === 'granted' || Notification.permission === undefined)) {
+      console.log('Color copiado correctamente!');
+    } else {
+      console.log('La notificación está desactivada.');
+    }
+
+    document.execCommand('copy');
+  }
+  }, [currentColor]);
+
+  const inputStyles: React.CSSProperties = {
+    textAlign: "center",
+    fontSize: "20px",
+    fontWeight: "600",
+    border: "none",
+    background: "transparent",
+    color: getContrastColor(currentColor),
+    cursor: "pointer",
+  }
+
+  const borderColor = currentColor
+  const transition = 'border-color 0.1s ease-in-out'
+
+  const buttonStyles: React.CSSProperties = {
+    border: `1px solid ${borderColor}`,
+    transition: transition,
+    cursor: 'pointer'
   }
 
   return (
     <div className="App">
-      <h1>Generador de colores</h1>
-      <div className="color__box" style={{ backgroundColor: currentColor }}>
-        <p style={{color: contrastColor}}>
+      <h1>Generador de color aleatorio</h1>
+      <div data-testid='color__box' className="color__box" style={{ backgroundColor: currentColor }}>
+        <p style={{ color: contrastColor }}>
           <input
+            data-testid="color-input"
             ref={hexRef}
-            type='text'
+            type="text"
             value={currentColor.toUpperCase()}
             readOnly
-            style={{
-              textAlign: 'center',
-              fontSize: '20px',
-              fontWeight: '600',
-              fontFamily: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif',
-              border: 'none',
-              background: 'transparent',
-              color: contrastColor,
-              cursor: 'pointer'
-            }}
+            style={inputStyles}
             onClick={handleCopyHex}
           />
         </p>
       </div>
       <button
+        data-testid="generate-color-button"
         onClick={handleGenerateColor}
-        onMouseEnter={() => setHoverColor(currentColor)}
-        onMouseLeave={() => setHoverColor(null)}
-        style={{
-          border: `1px solid ${hoverColor || currentColor}`,
-          transition: 'border-color 0.1s ease-in-out',
-          cursor: 'pointer'
-        }}>
-          generar
+        style={buttonStyles}
+      >
+        Generar color
       </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
