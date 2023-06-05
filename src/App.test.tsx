77c9caa87a15bb52bc32cb272a57360989e61f91
YeiserDeveloper <yeiser192@gmail.com>
2023-06-05 15:06:38 -0400

    Format documents

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,24 +1,24 @@
-import { render, screen, fireEvent } from "@testing-library/react";
-import App from "./App";
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from './App';
 
-describe("App", () => {
-  test("renders title", () => {
-    render(<App />);
-    const title = screen.getByText(/Generador de color aleatorio/i);
-    expect(title).toBeInTheDocument();
-  });
+describe('App', () => {
+	test('renders title', () => {
+		render(<App />);
+		const title = screen.getByText(/Generador de color aleatorio/i);
+		expect(title).toBeInTheDocument();
+	});
 
-  test("changes color on button click", () => {
-    render(<App />);
-    const generateButton = screen.getByText(/Generar color/i);
-    const colorBox = screen.getByTestId("color__box");
-    // Get initial color
-    const initialColor = colorBox.style.backgroundColor;
-    // Click button to generate new color
-    fireEvent.click(generateButton);
-    // Get new color
-    const newColor = colorBox.style.backgroundColor;
-    // Check that the color has changed
-    expect(newColor).not.toEqual(initialColor);
-  });
+	test('changes color on button click', () => {
+		render(<App />);
+		const generateButton = screen.getByText(/Generar color/i);
+		const colorBox = screen.getByTestId('color__box');
+		// Get initial color
+		const initialColor = colorBox.style.backgroundColor;
+		// Click button to generate new color
+		fireEvent.click(generateButton);
+		// Get new color
+		const newColor = colorBox.style.backgroundColor;
+		// Check that the color has changed
+		expect(newColor).not.toEqual(initialColor);
+	});
 });
